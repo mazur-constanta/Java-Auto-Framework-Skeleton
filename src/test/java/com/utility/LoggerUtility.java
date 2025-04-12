3f498672c1571f7e45d9046338aa707198860072
Constanta Mazur <ifcmazur@gmail.com>
2025-04-13 01:27:53 +0300

    fixed logging mapped to the Login page only

--- src/test/java/com/utility/LoggerUtility.java
+++ src/test/java/com/utility/LoggerUtility.java
@@ -4,12 +4,12 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class LoggerUtility {
-    public static Logger logger;
 
     private LoggerUtility() {
     }
 
     public static Logger getLogger(Class<?> clazz) {
+        Logger logger = null;
         if(logger == null) {
             logger = LogManager.getLogger(clazz);
         }
